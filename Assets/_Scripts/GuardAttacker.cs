eaab22f72fb690af96188d9f0b7118d354284b21
kuami6 <matiasskjetne@gmail.com>
2016-01-28 18:49:21 +0100

    Copied files from the old project

---
+++ Assets/_Scripts/GuardAttacker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardAttacker : MonoBehaviour {
+
+    public GameObject player;
+
+    void Start() {
+        player = GameObject.Find("Player");
+    }
+
+	void Update () {
+        if((transform.position - player.transform.position).sqrMagnitude < 81) {
+            if(Mathf.Abs(CalcAngle(player.transform.position - transform.position)) < 40) {
+                Debug.Log("Player in sight!!!");
+                player.transform.position = new Vector3(145.17f, 1f, 121.16f);
+            }
+        }
+    }
+
+    private float CalcAngle(Vector3 newDirection) {
+        Vector3 referenceForward = transform.forward;
+        Vector3 referenceRight = transform.right;
+        float angle = Vector3.Angle(newDirection, referenceForward);
+        float sign = Mathf.Sign(Vector3.Dot(newDirection, referenceRight));
+        return sign * angle;
+    }
+}
---
+++ Assets/_Scripts/ShootingTrigger.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootingTrigger : MonoBehaviour
+{
+	public GameObject arrow;
+
+	PlayerController playerController;
+
+	void Start ()
+	{
+		playerController = GameObject.Find ("Player").GetComponent<PlayerController> (); 
+	}
+
+
+	void Update ()
+	{
+	
+	}
+
+	void OnTriggerStay (Collider other)
+	{
+		Debug.Log ("somthing entered the trigger");
+
+		if (other.tag == "Player")
+		{
+			playerController.SnapToPoint (transform.position);
+			if (arrow != null)
+				arrow.SetActive (true);
+
+		}
+
+	}
+}
