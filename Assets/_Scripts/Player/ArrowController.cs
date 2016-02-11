e3f506a4444c6cce013a501fbd2412d823299455
kuami6 <matiasskjetne@gmail.com>
2016-02-11 16:27:15 +0100

    medium changes
    Commit Log
    
    Scripts
    Created a Guard class. - inherits from Actor
    modified the Actor class. - added variables for health
    modified the ArrwoController class. -added and removed a bunch of
    functions. the arrow does not work at this time
    
    Scene
    modified the "Prototype_CoreMechanics" scene

--- Assets/_Scripts/Player/ArrowController.cs
+++ Assets/_Scripts/Player/ArrowController.cs
@@ -6,43 +6,82 @@ public class ArrowController : MonoBehaviour
 {
 	Transform standardRotate;
 	Rigidbody arrowRigid;
+
 	public bool arrowFired;
+
+	Vector3 startRotation;
+	Vector3 calcRotation;
+	Vector3 Velocity;
+	Vector3 newVelocityDir;
+
 	float speed;
 	float lerpSpeed;
 	float force;
 
+	public float sensitivityX;
+	public float sensitivityY;
+	public float minimumX;
+	public float maximumX;
+	public float minimumY;
+	public float maximumY;
+	float rotationX;
+	float rotationY;
+
 	void Awake ()
 	{
-		arrowRigid = gameObject.GetComponent<Rigidbody> ();
-		arrowRigid.isKinematic = true;
-		standardRotate = transform;
+		arrowRigid = gameObject.GetComponent<Rigidbody> (); //find the arrows rigidbody
+		arrowRigid.isKinematic = true; //makes sure the arrow does not fall to the ground before it is fired
 	}
 
 
 	void Update ()
 	{
 		if (arrowFired)
-			transform.forward = Vector3.Slerp (transform.forward, arrowRigid.velocity.normalized, Time.deltaTime * 2f);
-
+		{
+			Velocity = arrowRigid.velocity;
+			transform.forward = Vector3.Slerp (transform.forward, arrowRigid.velocity.normalized, Time.deltaTime * 2f); //point the arrow in the direction of the velocity
+			ControlArrow ();
+		}
 	}
 
 	public void FireArrow (float force)
 	{
-		arrowRigid.isKinematic = false;
-		transform.SetParent (null);
-		arrowRigid.AddRelativeForce (Vector3.forward * force);
+		arrowRigid.isKinematic = false; //the arrow is now handeled by the physics engine
+		transform.SetParent (null); //makes the arrow independent of the camera movement
+		arrowRigid.AddRelativeForce (Vector3.forward * force); //fires the arrow in the arrows relative forward direction
 		arrowFired = true;
+		startRotation = transform.localEulerAngles;
+		Debug.Log ("the startRotation is: " + startRotation);
 	}
 
 	void OnCollisionEnter (Collision other)
 	{
-		arrowRigid.isKinematic = true;
+		arrowRigid.isKinematic = true; //when the arrow hits somthing, make it freeze in the air. Temp solution.
 
 	}
 
 	void ControlArrow ()
 	{
-		
+		rotationX = transform.localEulerAngles.y + Input.GetAxis ("Mouse X") * sensitivityX;
+
+		if (rotationX > 180)
+		{
+			rotationX -= 360;
+		}
+
+		if (rotationX <= minimumX)
+			rotationX = minimumX;
+
+		if (rotationX >= maximumX)
+			rotationX = maximumX;
+
+		Vector3 calcRotation = new Vector3 (startRotation.x * -rotationY, startRotation.y * rotationX, startRotation.z);
+
+		Debug.Log ("velocity before modify: " + arrowRigid.velocity);
+
+		arrowRigid.velocity = new Vector3 ((arrowRigid.velocity.x * calcRotation.x), arrowRigid.velocity.y, arrowRigid.velocity.z);
+
+		Debug.Log ("velocity after modify: " + arrowRigid.velocity);
 	}
 }
 	
