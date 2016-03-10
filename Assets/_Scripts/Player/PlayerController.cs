efc59ced15ff180f125bd05598efed82b1664299
Jonas Myhr Refseth <jonas@oisann.net>
2016-03-10 14:34:28 +0100

    fixed player controller

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -1,92 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof(QuestManager))]
-public class PlayerController : MonoBehaviour
-{
-	public enum CameraMode
-	{
-		First,
-		Third,
-		Arrow}
+public class PlayerController : MonoBehaviour {
+    public enum CameraMode { First, Third, Arrow };
+    public CameraMode currentCameraMode = CameraMode.Third;
 
-	;
+    public GameObject FirstPerson;
+    public GameObject ThirdPerson;
+    public GameObject ArrowMode;
 
-	public CameraMode currentCameraMode = CameraMode.Third;
+    private PlayerStats ps;
+    private GameObject arrowSpawn;
+    private Animator archerAni;
 
-	public GameObject FirstPerson;
-	public GameObject ThirdPerson;
-	public GameObject ArrowMode;
+    void Awake() {
+        ps = GetComponent<PlayerStats>();
+        arrowSpawn = FirstPerson.transform.FindChild("VerticalAnchor").FindChild("arm_with_bow").FindChild("ArrowSpawn").gameObject;
+        archerAni = ThirdPerson.transform.FindChild("Player").GetComponent<Animator>();
+    }
 
-	private PlayerStats ps;
-	private GameObject arrowSpawn;
+    void Update() {
+        if(isDead()) {
+            Debug.Log("Player died!");
+        }
+        if(currentCameraMode == CameraMode.Third) {
+            if (FirstPerson.activeInHierarchy)
+                FirstPerson.SetActive(false);
+            if (!ThirdPerson.activeInHierarchy)
+                ThirdPerson.SetActive(true);
 
-	void Awake ()
-	{
-		ps = GetComponent<PlayerStats> ();
-		arrowSpawn = FirstPerson.transform.FindChild ("VerticalAnchor").FindChild ("arm_with_bow").FindChild ("ArrowSpawn").gameObject;
-	}
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
 
-	void Update ()
-	{
-		if (isDead ())
-		{
-			Debug.Log ("Player died!");
-		}
-		if (currentCameraMode == CameraMode.Third)
-		{
-			if (FirstPerson.activeInHierarchy)
-				FirstPerson.SetActive (false);
-			if (!ThirdPerson.activeInHierarchy)
-				ThirdPerson.SetActive (true);
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                currentCameraMode = CameraMode.First;
+            }
+        } else if(currentCameraMode == CameraMode.First) {
+            if (!FirstPerson.activeInHierarchy)
+                FirstPerson.SetActive(true);
+            if (ThirdPerson.activeInHierarchy)
+                ThirdPerson.SetActive(false);
 
-			Cursor.visible = true;
-			Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
 
-			if (Input.GetKeyDown (KeyCode.Space))
-			{
-				currentCameraMode = CameraMode.First;
-			}
-		} else if (currentCameraMode == CameraMode.First)
-		{
-			if (!FirstPerson.activeInHierarchy)
-				FirstPerson.SetActive (true);
-			if (ThirdPerson.activeInHierarchy)
-				ThirdPerson.SetActive (false);
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                currentCameraMode = CameraMode.Third;
+            }
+            if (Input.GetMouseButtonDown(0)) {
+                currentCameraMode = CameraMode.Arrow;
+                FirstPerson.SetActive(false);
+                Transform vert = FirstPerson.transform.FindChild("VerticalAnchor");
+                GameObject arrow = Instantiate(ArrowMode, arrowSpawn.transform.position, arrowSpawn.transform.rotation) as GameObject;
+            }
+        } else {
+            if (FirstPerson.activeInHierarchy)
+                FirstPerson.SetActive(false);
+            if (ThirdPerson.activeInHierarchy)
+                ThirdPerson.SetActive(false);
 
-			Cursor.visible = false;
-			Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
 
-			if (Input.GetKeyDown (KeyCode.Space))
-			{
-				currentCameraMode = CameraMode.Third;
-			}
-			if (Input.GetMouseButtonDown (0))
-			{
-				currentCameraMode = CameraMode.Arrow;
-				FirstPerson.SetActive (false);
-				Transform vert = FirstPerson.transform.FindChild ("VerticalAnchor");
-				GameObject arrow = Instantiate (ArrowMode, arrowSpawn.transform.position, arrowSpawn.transform.rotation) as GameObject;
-			}
-		} else
-		{
-			if (FirstPerson.activeInHierarchy)
-				FirstPerson.SetActive (false);
-			if (ThirdPerson.activeInHierarchy)
-				ThirdPerson.SetActive (false);
+    public void takeDamage(float damage) {
+        ps.health -= Mathf.Clamp(damage, 0f, ps.health);
+    }
 
-			Cursor.visible = false;
-			Cursor.lockState = CursorLockMode.Locked;
-		}
-	}
-
-	public void takeDamage (float damage)
-	{
-		ps.health -= Mathf.Clamp (damage, 0f, ps.health);
-	}
-
-	public bool isDead ()
-	{
-		return ps.health <= 0f;
-	}
+    public bool isDead() {
+        return ps.health <= 0f;
+    }
 }
