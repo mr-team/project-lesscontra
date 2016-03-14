68ab934e3fb9c1a18ca91965bbffdd15914c2de6
Jonas Myhr Refseth <jonas@oisann.net>
2016-03-14 16:14:23 +0100

    win/lose screen

---
+++ Assets/_Scripts/Menus/ScreenController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenController : MonoBehaviour {
+    public PlayerStats ps;
+    public RectTransform LoseScreen;
+    public float deathScreenCooldown = .7f;
+
+    private float deathScreenTimer = 0f;
+
+    void Update() {
+        if(ps.health <= 0f) {
+            deathScreenTimer += Time.deltaTime;
+            if(deathScreenTimer >= deathScreenCooldown) {
+                LoseScreen.gameObject.SetActive(true);
+            }
+        }
+    }
+}
